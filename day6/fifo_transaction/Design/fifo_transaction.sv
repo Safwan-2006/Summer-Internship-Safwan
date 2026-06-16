9b5a7a079b0c76b9be6540ca7667b485bfcd6b45
Safwan-2006 <mohammedsafwansf@gmail.com>
2026-06-16 21:24:39 +0530

    Add files via upload

---
+++ day6/fifo_transaction/Design/fifo_transaction.sv
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.06.2026 21:16:02
+// Design Name: 
+// Module Name: fifo_transaction
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//fifo transaction
+class fifo_transaction;
+  
+  rand bit [7:0] data_in;
+  rand bit wr_enb;
+  rand bit rd_enb;
+  
+  bit [7:0] data_out;
+  bit empty;
+  bit full;
+  
+  int min;
+  int max;
+  
+  // User-defined data boundary constraint
+  constraint c_1 {
+    data_in > min;
+    data_in < max;
+  }
+  
+  // Prevents simultaneous read and write
+  constraint c_2 { 
+    !(wr_enb == 1 && rd_enb == 1);
+  }
+  
+  // Ensures active reading and writing
+  constraint c_3 {
+    wr_enb dist {1 := 50, 0 := 50};
+    rd_enb dist {1 := 50, 0 := 50};
+  }
+  
+  
+  function void set_boundaries(int min_value, int max_value);
+    this.min = min_value;
+    this.max = max_value;
+  endfunction
+    
+  function void post_randomize();
+    $display("Transaction Randomized -> WR: %0b | RD: %0b | DATA_IN: %0d", 
+              wr_enb, rd_enb, data_in);
+  endfunction
+    
+  function void display(string s = "FIFO_TRANS");
+    $display("[%s] WR=%0b RD=%0b DIN=%0d DOUT=%0d FULL=%0b EMPTY=%0b", 
+              s, wr_enb, rd_enb, data_in, data_out, full, empty);
+  endfunction  
+    
+endclass
+
+module tb;
+  
+  fifo_transaction tx;
+
+  initial begin
+    tx = new();
+    tx.set_boundaries(2, 50);
+
+	for (int i = 0; i < 10; i++) begin
+      if (tx.randomize())
+        $display("Value of wr_enb is %0b, rd_enb is %0b, data_in is %0d",tx.wr_enb, tx.rd_enb, tx.data_in);
+      else
+        $display("Randomization failed");
+    end
+      
+  end
+
+endmodule
